f62f12452127339bf1689eaa2d5fb43ecbcb987f
rakeshsp <rakesh.jnv21@gmail.com>
2025-11-28 05:16:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-deep": "#020617",
+      },
+      boxShadow: {
+        glass: "0 18px 45px rgba(15,23,42,0.7)",
+      },
+    },
   },
   plugins: [],
 };
